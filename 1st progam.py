aa463ac4288d04e8ea3aaf762473c72e8951b14f
Himnees <himneeshprasad124@gmail.com>
2023-07-31 19:10:42 +0530

    First day

---
+++ 1st progam.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+c=a+b
+print(c)
+print("sum= ",(a+b))
+print("sum= ",(a+a))
+print("sum= ",(a+b))
+print("sum= ",(b+b))
---
+++ docstring.py
@@ -0,0 +1,17 @@
+#Docstring
+#Program with two function
+def add(x,y):
+    """
+    This function takes two numbers and finds their sum,
+    It displays the sum as result
+    """
+    print("sum = ",(x+y))
+    
+def message():
+    print("Welcome to python")
+    ''' This function displays a message.
+    This is a welcome message to the user
+    '''
+     
+add(10,25)
+message()
